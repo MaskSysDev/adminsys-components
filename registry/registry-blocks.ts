1720fffb3c8d4549e70e2d6536bd1d8b2adf19b0
Mask <marcosp.musical@gmail.com>
2024-10-09 08:28:54 -0300

    feat: add components

--- registry/registry-blocks.ts
+++ registry/registry-blocks.ts
@@ -491,6 +491,7 @@ export const blocks: Registry = [
       "card",
       "sonner",
       "toast",
+      "https://raw.githubusercontent.com/MaskSysDev/adminsys-components/refs/heads/main/public/r/styles/default/not-found.json",
       "https://raw.githubusercontent.com/MaskSysDev/adminsys-components/refs/heads/main/public/r/styles/default/next-themes.json",
       "https://raw.githubusercontent.com/MaskSysDev/adminsys-components/refs/heads/main/public/r/styles/default/logo.json",
       "https://raw.githubusercontent.com/MaskSysDev/adminsys-components/refs/heads/main/public/r/styles/default/theme-adminsys.json",
